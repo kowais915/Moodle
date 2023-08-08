dd597d5c92365eeb7d9f356458f7bdfdfcdd8ca7
kowais915 <kowais915@gmail.com>
2023-08-08 23:52:30 +0500

    :iphone: Made the hero responsive

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 const Hero = () => {
     return ( 
         <>
-           <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+           <div className=" hero sm:min-h-screen w-fit sm:w-full bg-base-200 ">
+                <div className="hero-content flex-col lg:flex-row-reverse flex flex-row ">
                     <img src="img.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                    <h1 className="text-5xl font-bold">Moodle - your mood tracker.</h1>
-                    <p className="py-6">Your ultimate solution to keep track of your changing moods.</p>
-                    <Link href="/dashboard" className="btn btn-primary">Get Started</Link>
+                    <div className='text-center sm:text-start'>
+                    <h1 className="text-center text-md text-5xl font-bold sm:text-start">Moodle - your mood tracker.</h1>
+                    <p className="py-6 text-center sm:text-start">Your ultimate solution to keep track of your changing moods.</p>
+                    <Link href="/dashboard" className="btn btn-primary ">Get Started</Link>
                     </div>
                 </div>
             </div>
